b777b55a54c59753b7f44dd47d5b909ab87d4cf4
Ilina <ilina.tsoneva3@gmail.com>
2019-11-03 21:10:31 +0200

    Update

---
+++ CSharpOOPExercises/WorkingWithAbstraction/CarsSalesman/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace CarsSalesman
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var runner = new Runner();
+            runner.Run();
+        }
+    }
+}
