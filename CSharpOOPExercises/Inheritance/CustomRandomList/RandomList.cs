ee8b73f85a02f1a29abc78e4090aebb4567e505f
Ilina <ilina.tsoneva3@gmail.com>
2019-11-06 00:15:46 +0200

    Inheritance lab

---
+++ CSharpOOPExercises/Inheritance/CustomRandomList/RandomList.cs
@@ -0,0 +1,24 @@
+﻿namespace CustomRandomList
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class RandomList : List<string>
+    {
+        private Random rnd;
+
+        public RandomList()
+        {
+            this.rnd = new Random();
+        }
+
+        public string RandomString()
+        {
+            int rndIndexToRemove = this.rnd.Next(0, this.Count);
+            string result = this[rndIndexToRemove];
+            this.RemoveAt(rndIndexToRemove);
+            return result;
+        }
+    }
+}
---
+++ CSharpOOPExercises/Inheritance/Farm/Animal.cs
@@ -0,0 +1,11 @@
+﻿namespace Farm
+{
+    using System;
+    public abstract class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eating...");
+        }
+    }
+}
