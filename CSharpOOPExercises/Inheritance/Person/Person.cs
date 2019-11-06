bbf53e6a55ef9c406b2d64e6fc2f2187c6f7d88a
Ilina <ilina.tsoneva3@gmail.com>
2019-11-06 20:39:04 +0200

    First half of Inheritance added

---
+++ CSharpOOPExercises/Inheritance/Person/Person.cs
@@ -0,0 +1,38 @@
+﻿namespace Person
+{
+    using System;
+    using System.Text;
+
+    public class Person
+    {
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name { get; set; }
+
+        public virtual int Age
+        {
+            get => this.age;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Age must be a positive number");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Name: {this.Name}, Age: {this.Age}");
+            return sb.ToString();
+        }
+    }
+}
