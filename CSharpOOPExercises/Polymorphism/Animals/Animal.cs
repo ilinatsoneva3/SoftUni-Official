3a3604e6279500fc416c8ef443e0ee0e5dbcf72c
Ilina <ilina.tsoneva3@gmail.com>
2019-11-13 22:49:12 +0200

    Polymorphism

---
+++ CSharpOOPExercises/Polymorphism/Animals/Animal.cs
@@ -0,0 +1,19 @@
+﻿namespace Animals
+{
+    public class Animal
+    {
+        public Animal(string name, string favoriteFood)
+        {
+            this.Name = name;
+            this.FavoriteFood = favoriteFood;
+        }
+
+        public string Name { get; protected set; }
+        public string FavoriteFood { get; protected set; }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my fovourite food is {this.FavoriteFood}";
+        }
+    }
+}
