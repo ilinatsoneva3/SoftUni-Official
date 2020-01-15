d64ace4d936354fb2212183ba7af73fd5b105b90
Ilina <ilina.tsoneva3@gmail.com>
2020-01-15 21:16:38 +0200

    Update

---
+++ JSAdvancedExercises/Arrays/NegativePositiveNumbers.js
@@ -0,0 +1,14 @@
+function solve(arr) {
+    let result = [];
+
+    for (const number of arr) {
+        if (number < 0) {
+            result.unshift(number);
+        } else {
+            result.push(number);
+        }
+    }
+    console.log(result.join('\n'));
+}
+
+solve([3, -2, 0, -1]);
