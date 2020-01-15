4cb888ddca2a09b9b0fbf02fe97585e3383c2140
Ilina <ilina.tsoneva3@gmail.com>
2020-01-15 20:37:07 +0200

    JS

---
+++ JSExercises/Arrays/EvenPositionElement.js
@@ -0,0 +1,14 @@
+function solve(arr){
+    let result = [];
+
+    for (let i = 0; i<arr.length;i++) {
+
+        if(i%2===0){
+        result.push(arr[i]);
+        }
+    }
+
+    console.log(result.join(' '));
+}
+
+solve(['JS']);
