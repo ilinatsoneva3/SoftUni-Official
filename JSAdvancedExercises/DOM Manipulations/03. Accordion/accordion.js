2847b913b339e15e5e53c0ef16b9a64f6040ab30
Ilina <ilina.tsoneva3@gmail.com>
2020-02-07 19:17:21 +0200

    update

--- JSAdvancedExercises/DOM Manipulations/03. Accordion/accordion.js
+++ JSAdvancedExercises/DOM Manipulations/03. Accordion/accordion.js
@@ -1,14 +1,12 @@
 function toggle() {
-    function toggle() {
-        let button = document.querySelector('.button');
-        let text = document.getElementById('extra');
-    
-        if (text.style.display === 'none') {
-            text.style.display = 'block';
-            button.textContent='Less';
-        } else {
-            text.style.display = 'none';
-            button.textContent='More';
-        }
+    let button = document.querySelector('.button');
+    let text = document.getElementById('extra');
+
+    if (text.style.display === 'none') {
+        text.style.display = 'block';
+        button.textContent = 'Less';
+    } else {
+        text.style.display = 'none';
+        button.textContent = 'More';
     }
 }
