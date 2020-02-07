8d3a74ac7f1d073db072c21fb6502dee0a7fa525
Ilina <ilina.tsoneva3@gmail.com>
2020-02-07 19:18:32 +0200

    update

--- JSAdvancedExercises/DOM Manipulations/05. Notification/scripts.js
+++ JSAdvancedExercises/DOM Manipulations/05. Notification/scripts.js
@@ -1,3 +1,10 @@
 function notify(message) {
-    console.log('TODO:...');
+    let button = document.getElementsByTagName('button')[0];
+    let notification = document.getElementById('notification');
+    notification.textContent = message;
+    notification.style.display = 'block';
+
+    setTimeout(() => {
+        notification.style.display = 'none';
+    }, 2000);
 }
