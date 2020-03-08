2a1eef3c4e393b641cb0919272efbe7173d640bb
Ilina <ilina.tsoneva3@gmail.com>
2020-03-08 20:53:03 +0200

    Update

---
+++ JSAppsExercises/Prototype and Inheritance/baloons.js
@@ -0,0 +1,44 @@
+function baloons() {
+  class Balloon {
+    constructor(color, gasWeight) {
+      this.color = color;
+      this.gasWeight = gasWeight;
+    }
+  }
+
+  class PartyBalloon extends Balloon {
+    constructor(color, gasWeight, ribbonColor, ribbonLength) {
+      super(color, gasWeight);
+      this._ribbon = {
+        color: ribbonColor,
+        length: ribbonLength
+      };
+    }
+
+    get ribbon() {
+      return this._ribbon;
+    }
+  }
+
+  class BirthdayBalloon extends PartyBalloon {
+    constructor(color, gasWeight, ribbonColor, ribbonLength, text) {
+      super(color, gasWeight, ribbonColor, ribbonLength);
+      this._text = text;
+    }
+
+    get text() {
+      return this._text;
+    }
+  }
+
+  let test = new PartyBalloon("blue", 1, "white", 2);
+  console.log(test.ribbon.color);
+
+  //return {
+  //  Balloon,
+  //  PartyBalloon,
+  //  BirthdayBalloon
+  //};
+}
+
+baloons();
