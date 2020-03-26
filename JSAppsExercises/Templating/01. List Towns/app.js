681a13917f4c8a4a973d1086510c58feea8521d5
Ilina <ilina.tsoneva3@gmail.com>
2020-03-26 22:06:01 +0200

    Update

--- JSAppsExercises/Templating/01. List Towns/app.js
+++ JSAppsExercises/Templating/01. List Towns/app.js
@@ -5,14 +5,17 @@
   btn.addEventListener("click", listTowns);
 
   async function listTowns() {
-    let towns = townsInfo.value.split(", ");
+    document.getElementById("root").innerHTML = "";
+    if (townsInfo.value) {
+      let towns = townsInfo.value.split(", ");
 
-    let townsTemplate = await fetch("./towns-template.hbs").then(res =>
-      res.text()
-    );
-    let template = Handlebars.compile(townsTemplate);
-    let list = template({ towns });
-    document.getElementById("root").innerHTML = list;
-    document.getElementById("towns").value = "";
+      let townsTemplate = await fetch("./towns-template.hbs").then(res =>
+        res.text()
+      );
+      let template = Handlebars.compile(townsTemplate);
+      let list = template({ towns });
+      document.getElementById("root").innerHTML = list;
+      document.getElementById("towns").value = "";
+    }
   }
 })();
