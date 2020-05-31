9d91e74a2101eca8f5e7fb97f54263272ac8e046
Ilina <ilina.tsoneva3@gmail.com>
2020-05-31 20:47:16 +0300

    Table relations

---
+++ Database Basics - MSSQL Server/Table Relations/SQLQuery1.sql
@@ -0,0 +1,200 @@
+--TASK ONE--
+
+CREATE TABLE Passports
+(
+	PassportID INT PRIMARY KEY IDENTITY(101,1),
+	PassportNumber CHAR(8) UNIQUE
+)
+
+CREATE TABLE Persons
+(
+	PersonID INT PRIMARY KEY IDENTITY NOT NULL,
+	FirstName NVARCHAR(30) NOT NULL,
+	Salary DECIMAL(7,2),
+	PassportID INT FOREIGN KEY REFERENCES Passports(PassportID)
+)
+
+INSERT INTO Passports(PassportNumber)
+	VALUES ('N34FG21B'), ('K65LO4R7'), ('ZE657QP2')
+
+INSERT INTO Persons(FirstName, Salary, PassportID)
+	VALUES ('Roberto', 43300.00, 102),
+			('Tom', 56100.00, 103),
+			('Yana', 60200.00, 101)
+
+--TASK TWO--
+
+CREATE TABLE Manufacturers
+(
+	ManufacturerID INT PRIMARY KEY IDENTITY NOT NULL,
+	[Name] NVARCHAR(50) NOT NULL,
+	EstablishedOn DATETIME2
+)
+
+INSERT INTO Manufacturers([Name],EstablishedOn)
+	VALUES ('BMW', '1916-03-07'),
+			('Tesla', '2003-01-01'),
+			('Lada', '1966-05-01')
+
+CREATE TABLE Models
+(
+	ModelID INT PRIMARY KEY IDENTITY(101,1) NOT NULL,
+	[Name] NVARCHAR(50) NOT NULL,
+	ManufacturerID INT NOT NULL FOREIGN KEY REFERENCES Manufacturers(ManufacturerID)
+)
+
+INSERT INTO Models([Name], ManufacturerID)
+	VALUES ('X1', 1),
+			('I6', 1),
+			('Model S', 2),
+			('Model X', 2),
+			('Model 3', 2),
+			('Nova', 3)
+
+--TASK THREE--
+
+CREATE TABLE Students
+(
+	StudentID INT PRIMARY KEY IDENTITY NOT NULL,
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Exams
+(
+	ExamID INT PRIMARY KEY IDENTITY(101,1) NOT NULL,
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+INSERT INTO Students([Name])
+	VALUES ('Mila'), ('Toni'), ('Ron')
+
+INSERT INTO Exams([Name])
+	VALUES	('SpringMVC'), ('Neo4j'), ('Oracle 11g')
+
+CREATE TABLE StudentsExams
+(
+	StudentID INT NOT NULL FOREIGN KEY REFERENCES Students(StudentID),
+	ExamID INT NOT NULL FOREIGN KEY REFERENCES Exams(ExamID),
+	PRIMARY KEY(StudentID, ExamID)
+)
+
+INSERT INTO StudentsExams(StudentID, ExamID)
+	VALUES (1, 101),
+			(1, 102),
+			(2, 101),			
+			(3, 103),
+			(2, 102),
+			(2, 103)
+
+
+--TASK FOUR--
+
+CREATE TABLE Teachers
+(
+	TeacherID INT NOT NULL PRIMARY KEY IDENTITY(101,1),
+	[Name] NVARCHAR(50) NOT NULL,
+	ManagerID INT FOREIGN KEY REFERENCES Teachers(TeacherID)
+)
+
+INSERT INTO Teachers([Name])
+	VALUES ('John'), ('Maya'), ('Silvia'), ('Ted'), ('Mark'), ('Greta')
+
+UPDATE Teachers
+	SET ManagerID = 106
+	WHERE TeacherID IN (102, 103)
+
+UPDATE Teachers
+	SET ManagerID = 101
+	WHERE TeacherID IN (105, 106)
+
+UPDATE Teachers
+	SET ManagerID = 105
+	WHERE TeacherID = 104
+
+
+--TASK FIVE--
+
+CREATE TABLE ItemTypes
+(
+	ItemTypeID INT NOT NULL PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Items
+(
+	ItemID INT NOT NULL PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(50) NOT NULL,
+	ItemTypeID INT NOT NULL FOREIGN KEY REFERENCES ItemTypes(ItemTypeID)
+)
+
+CREATE TABLE Cities
+(
+	CityID INT NOT NULL PRIMARY KEY IDENTITY,	
+	[Name] VARCHAR(50) NOT NULL,
+)
+
+CREATE TABLE Customers
+(
+	CustomerID INT NOT NULL PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(50) NOT NULL,
+	Birthday DATE,
+	CityID INT NOT NULL FOREIGN KEY REFERENCES Cities(CityID)
+)
+
+CREATE TABLE Orders
+(
+	OrderID INT NOT NULL PRIMARY KEY IDENTITY,
+	CustomerID INT NOT NULL FOREIGN KEY REFERENCES Customers(CustomerID)
+)
+
+CREATE TABLE OrderItems
+(
+	OrderID INT NOT NULL FOREIGN KEY REFERENCES Orders(OrderID),
+	ItemID INT NOT NULL FOREIGN KEY REFERENCES Items(ItemID),
+	PRIMARY KEY(OrderID, ItemID)
+)
+
+--TASK SIX--
+
+CREATE TABLE Majors
+(
+	MajorID INT NOT NULL PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Students
+(
+	StudentID INT NOT NULL PRIMARY KEY IDENTITY,
+	StudentNumber CHAR(6) NOT NULL UNIQUE,
+	StudentName NVARCHAR(50) NOT NULL,
+	MajorID INT NOT NULL FOREIGN KEY REFERENCES Majors(MajorID)
+)
+
+CREATE TABLE Payments
+(
+	PaymentID INT NOT NULL PRIMARY KEY IDENTITY,
+	PaymentDate DATE NOT NULL,
+	PaymentAmount DECIMAL(7,2) NOT NULL,
+	StudentID INT NOT NULL FOREIGN KEY REFERENCES Students(StudentID)
+)
+
+CREATE TABLE Subjects
+(
+	SubjectID INT NOT NULL PRIMARY KEY IDENTITY,
+	SubjectName NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Agenda
+(
+	StudentID INT NOT NULL FOREIGN KEY REFERENCES Students(StudentID),
+	SubjectID INT NOT NULL FOREIGN KEY REFERENCES Subjects(SubjectID),
+	PRIMARY KEY(StudentID, SubjectID)
+)
+
+--TASK NINE--
+
+SELECT m.MountainRange, p.PeakName, p.Elevation
+	FROM Peaks AS p
+	JOIN Mountains AS m ON p.MountainId = m.Id
+	WHERE m.MountainRange = 'Rila'
+	ORDER BY p.Elevation DESC
