965cd169a238a71c0be74311d50b4609a4e6a71a
Ilina <ilina.tsoneva3@gmail.com>
2020-06-02 18:52:05 +0300

    update

---
+++ C# Advanced Exam/BookWorm/StartUp.cs
@@ -0,0 +1,103 @@
+﻿namespace BookWorm
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Linq;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var result = new LinkedList<char>(Console.ReadLine().ToCharArray());
+            var rows = int.Parse(Console.ReadLine());
+            var field = new char[rows][];
+            var playerRow = 0;
+            var playerCol = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                field[row] = Console.ReadLine().ToCharArray();
+
+                for (int col = 0; col < field[row].Length; col++)
+                {
+                    if (field[row][col] == 'P')
+                    {
+                        playerRow = row;
+                        playerCol = col;
+                    }
+                }
+            }
+
+            var command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                bool isOutside = true;
+
+                switch (command)
+                {
+                    case "left":
+                        isOutside = CheckBoundaries(playerRow, playerCol - 1, field);
+                        break;
+                    case "right":
+                        isOutside = CheckBoundaries(playerRow, playerCol + 1, field);
+                        break;
+                    case "up":
+                        isOutside = CheckBoundaries(playerRow - 1, playerCol, field);
+                        break;
+                    case "down":
+                        isOutside = CheckBoundaries(playerRow + 1, playerCol, field);
+                        break;
+                }
+
+                if (!isOutside)
+                {
+                    if (result.Any())
+                    {
+                        result.RemoveLast();
+                    }
+                }
+                else
+                {
+                    field[playerRow][playerCol] = '-';
+
+                    switch (command)
+                    {
+                        case "left":
+                            playerCol -= 1;
+                            break;
+                        case "right":
+                            playerCol += 1;
+                            break;
+                        case "up":
+                            playerRow -= 1;
+                            break;
+                        case "down":
+                            playerRow += 1;
+                            break;
+                    }
+
+                    if (char.IsLetter(field[playerRow][playerCol]))
+                    {
+                        result.AddLast(field[playerRow][playerCol]);
+                    }
+
+                    field[playerRow][playerCol] = 'P';
+                }
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join("", result));
+
+            for (int row = 0; row < rows; row++)
+            {
+                Console.WriteLine(string.Join("", field[row]));
+            }
+        }
+        private static bool CheckBoundaries(int row, int col, char[][] field)
+        {
+            return row >= 0 && col >= 0 && row < field.Length && col < field[row].Length;
+        }
+    }
+}
---
+++ C# Advanced Exam/Rabbits/Cage.cs
@@ -0,0 +1,84 @@
+﻿namespace Rabbits
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Linq;
+
+    class Cage
+    {
+        private List<Rabbit> rabbits;
+        public Cage(string name, int capacity)
+        {
+            this.rabbits = new List<Rabbit>();
+            this.Name = name;
+            this.Capacity = capacity;
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        public int Count => this.rabbits.Count;
+
+        public void Add(Rabbit rabbit)
+        {
+            if (this.rabbits.Count < this.Capacity)
+            {
+                this.rabbits.Add(rabbit);
+            }
+        }
+
+        public bool RemoveRabbit(string name)
+        {
+            Rabbit rabbit = this.rabbits.Where(r => r.Name == name).FirstOrDefault();
+
+            if (rabbit != null)
+            {
+                this.rabbits.Remove(rabbit);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void RemoveSpecies(string species)
+        {
+            this.rabbits = this.rabbits.Where(r => r.Species != species).ToList();
+        }
+
+        public Rabbit SellRabbit(string name)
+        {
+            Rabbit rabbit = this.rabbits.Where(r => r.Name == name).FirstOrDefault();
+            rabbit.Available = false;
+            return rabbit;
+        }
+
+        public Rabbit[] SellRabbitsBySpecies(string species)
+        {
+            Rabbit[] rabbitsBySpecies = this.rabbits.Where(r => r.Species == species).ToArray();
+
+            foreach (var rabbit in rabbitsBySpecies)
+            {
+                rabbit.Available = false;
+            }
+
+            return rabbitsBySpecies;
+        }
+
+        public string Report()
+        {
+            List<Rabbit> rabbitsToPrint = this.rabbits.Where(r => r.Available == true).ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Rabbits available at {this.Name}:");
+
+            foreach (var rabbit in rabbitsToPrint)
+            {
+                sb.AppendLine(rabbit.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
