756cae07e7a443c98a6d9717fe4ff7db0a72dabf
Ilina <ilina.tsoneva3@gmail.com>
2020-06-02 18:48:22 +0300

    update

---
+++ Calculator App/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CalculatorApp.Models;
+
+namespace CalculatorApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(Calculator calculator)
+        {
+            return View(calculator);
+        }
+        [HttpPost]
+        public IActionResult Calculate(Calculator calculator)
+        {
+            calculator.CalculateResult();
+
+            return RedirectToAction("Index", calculator);
+        }
+
+    }
+}
