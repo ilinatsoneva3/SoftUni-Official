e36bb3ddf72935e9ca06a4ed338483e7e2598fa3
Ilina <ilina.tsoneva3@gmail.com>
2020-06-02 20:28:03 +0300

    Recursive drawing

---
+++ Algorithms/Recursion/RecursiveDrawing/RecursiveDrawing.cs
@@ -0,0 +1,32 @@
+﻿namespace RecursiveDrawing
+{
+    using System;
+
+    class RecursiveDrawing
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(new string('*', number));
+            PrintFigure(number-1);
+            Console.WriteLine(new string('#', number));
+            
+        }
+
+       
+        private static void PrintFigure(int number)
+        {
+            if (number==0)
+            {
+                return;
+            }
+
+            Console.WriteLine(new string('*', number));
+
+            PrintFigure(number - 1);
+
+            Console.WriteLine(new string('#', number));
+        }
+    }
+}
