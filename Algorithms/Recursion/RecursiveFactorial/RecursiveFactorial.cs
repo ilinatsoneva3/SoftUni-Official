e671580da11a93e1291a96c49868dd30f6ec1c41
Ilina <ilina.tsoneva3@gmail.com>
2020-06-02 19:29:45 +0300

    Calculate factorial recursively

---
+++ Algorithms/Recursion/RecursiveFactorial/RecursiveFactorial.cs
@@ -0,0 +1,24 @@
+﻿namespace RecursiveFactoriel
+{
+    using System;
+    class RecursiveFactorial
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            var result = CalculateFactorial(number);
+            Console.WriteLine(result);
+        }
+
+        private static int CalculateFactorial(int number)
+        {
+            if (number == 1)
+            {
+                return 1;
+            }
+
+            return number * CalculateFactorial(number - 1);
+        }
+    }
+}
