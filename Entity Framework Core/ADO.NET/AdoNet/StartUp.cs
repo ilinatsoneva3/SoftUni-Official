04223d32332ae93f2aeeca6fd2146f164d0c6e8f
Ilina <ilina.tsoneva3@gmail.com>
2020-06-22 22:39:13 +0300

    FirstTask

---
+++ Entity Framework Core/ADO.NET/AdoNet/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace VilainNames
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using var sqlConnection = new SqlConnection(@"Server=DESKTOP-URSLOO9\SQLEXPRESS;Database=MinionsDB;Integrated Security=true");
+
+            sqlConnection.Open();
+
+            var command = new SqlCommand("SELECT Name, COUNT(VillainId) AS MinionsCount "  +
+                                            "FROM Villains " +
+                                            "JOIN MinionsVillains ON Id = VillainId " +
+                                        "GROUP BY Id, Name " +
+                                          "HAVING COUNT(VillainId) > 3 " +
+                                        "ORDER BY COUNT(VillainId) DESC", sqlConnection);
+
+            using var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                var name = reader["Name"];
+                var count = reader["MinionsCount"];
+                Console.WriteLine($"{name} - {count}");
+            }
+        }
+    }
+}
