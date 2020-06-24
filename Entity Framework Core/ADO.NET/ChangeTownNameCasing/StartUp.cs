68b375312dd6aeb2c8e8204fde526013a0167c37
Ilina <ilina.tsoneva3@gmail.com>
2020-06-24 09:12:32 +0300

    Task four

---
+++ Entity Framework Core/ADO.NET/ChangeTownNameCasing/StartUp.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Data.SqlClient;
+
+namespace ChangeTownNameCasing
+{
+    class StartUp
+    {
+        private const string connectionText = @"Server=DESKTOP-URSLOO9\SQLEXPRESS;Database=MinionsDB;Integrated Security=true";
+
+        static void Main(string[] args)
+        {
+            using var connection = new SqlConnection(connectionText);
+
+            connection.Open();
+
+            var country = Console.ReadLine();
+
+            //find country id
+
+            var queryText = @"SELECT Id FROM [Countries] WHERE [Name] = @countryName";
+            var sqlCommand = new SqlCommand(queryText, connection);
+            sqlCommand.Parameters.AddWithValue("@countryName", country);
+            var countryId = sqlCommand.ExecuteScalar()?.ToString();
+
+            //find if country exists in database
+
+            var result = new StringBuilder();
+
+            if (countryId is null)
+            {
+                result.AppendLine("No town names were affected.");
+            }
+            else
+            {
+                //change names to upper case
+                var id = int.Parse(countryId);
+
+                queryText = @"UPDATE Towns
+                               SET Name = UPPER(Name)
+                             WHERE CountryCode = @countryId";
+
+                sqlCommand = new SqlCommand(queryText, connection);
+                sqlCommand.Parameters.AddWithValue("@countryId", id);
+                var numOfNames = sqlCommand.ExecuteNonQuery().ToString();
+                result.AppendLine($"{numOfNames} town names were affected. ");
+
+                //get names of towns
+                queryText = @"SELECT [Name] FROM Towns WHERE CountryCode = @countryId";
+                sqlCommand = new SqlCommand(queryText, connection);
+                sqlCommand.Parameters.AddWithValue("@countryId", id);
+
+                var towns = new List<string>();
+
+                using var reader = sqlCommand.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    towns.Add(reader["Name"].ToString());
+                }
+
+                result.AppendLine(string.Join(", ", towns));
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
